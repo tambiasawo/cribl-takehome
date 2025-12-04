1e9f2011d2e32a05f92601e4fdfc41e9795ab6f5
tambiasawo <tambi.asawo@yahoo.com>
2025-12-03 23:35:38 -0800

    feat: call fetchlogs and render app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-import { useState } from "react";
+import useLogStream from "./hooks/useLogStream";
+import LogTable from "./components/LogTable";
+import styles from "./App.module.css";
 
-import "./App.css";
+const URL = "https://s3.amazonaws.com/io.cribl.c021.takehome/cribl.log";
 
 function App() {
-  return <div>hello world</div>;
+  const { events, isLoading, error, isDone } = useLogStream(URL);
+
+  return (
+    <div className={styles.appRoot}>
+      <header className={styles.appHeader}>
+        <h1>Take-Home Log Viewer</h1>
+
+        <div className={styles.appStatus}>
+          {error && <span className={styles.statusBadgeError}>Error</span>}
+          {!error && isLoading && !isDone && (
+            <span className={styles.statusBadgeLoading}>Streaming...</span>
+          )}
+          {!error && !isLoading && isDone && (
+            <span className={styles.statusBadgeDone}>Finished</span>
+          )}
+        </div>
+      </header>
+
+      {error && (
+        <div className={styles.appMessageError}>
+          Failed to load logs: {error}
+        </div>
+      )}
+
+      {!error && (events.length > 0 || isLoading) && (
+        <LogTable events={events} />
+      )}
+
+      {!error && !isLoading && isDone && events.length === 0 && (
+        <div className={styles.appMessage}>No log events found.</div>
+      )}
+    </div>
+  );
 }
 
 export default App;
